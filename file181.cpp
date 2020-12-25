f7070fdda427c25558a51866ba92a72de80af0a9
Shubham Verma <var.subham@gmail.com>
2020-12-25 23:27:35 +0530

    subarray sum equal to k

--- file181.cpp
+++ file181.cpp
@@ -1,12 +1,40 @@
+// subarray sum equal K
+
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+class Solution{
+    public:
+        int subarraySum(vector<int> &nums, int k){
+            int ans = 0, cum_sum = 0;
+
+            unordered_map<int, int> pref_sum;
+
+            for (int i : nums)
+            {
+
+                cum_sum += i;
+
+                int req = cum_sum - k;
+
+                if (req == 0)
+                    ans++;
+
+                ans += pref_sum.count(req) == 1 ? pref_sum[req] : 0;
+
+                pref_sum[cum_sum]++;
+            }
+
+            return ans;
+        }
+};
+
 int main(){
-    vector<int> vt = {2,45,76,3,2,9,5,2};
-    priority_queue<int> pq(vt.begin(), vt.end());
-    pq.pop();
-    cout << pq.top() << endl;
+    Solution st;
+    vector<int> nums = {1,2,3};
+    int k = 3;
+    cout << st.subarraySum(nums, k) << endl;
 }
