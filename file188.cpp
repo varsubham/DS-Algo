aca4a45860fc8aeca50b4bb1fdefbd1d178d6999
varsubham <var.subham@gmail.com>
2021-01-16 16:48:27 +0530

    kth largest element

---
+++ file188.cpp
@@ -0,0 +1,26 @@
+// kth largest element
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int> pq(nums.begin(), nums.end());
+        while(k > 1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
+
+int main(){
+    Solution st;
+    vector<int> vt = {3,2,3,1,2,4,5,5,6};
+    int k = 4;
+    cout << st.findKthLargest(vt, k) << endl;
+}
